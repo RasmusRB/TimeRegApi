5f4699aaf3f486efb04f6a4781f0a87e565798a2
Nikolai Køhler Lundberg Hansen <niko9946@edu.easj.dk>
2021-12-06 12:17:42 +0100

    nikolaj - activityModel og migration

---
+++ Migrations/20211206092856_create_activity_table.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace TimeReg_Api.Migrations
+{
+    public partial class create_activity_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+             migrationBuilder.CreateTable(
+              name: "activities",
+              columns: table => new
+              {
+                  id = table.Column<long>(type: "bigint", nullable: false).Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                  activity = table.Column<string>(type: "text", nullable: true),
+              },
+              constraints: table =>
+              {
+                  table.PrimaryKey("PK_Activity", x => x.id);
+              }
+            );
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ TimeRegApp/Model/Activity/ActivityModel.cs
@@ -0,0 +1,8 @@
+namespace TimeRegApi.TimeRegApp.Model.Activity
+{
+    public class ActivityModel
+    {
+        public int Activity_Id { get; set; }
+        public string Activity_Category { get; set; }
+    }
+}
